da1a17ce5f8066bc11074d0752625088833053f3
Lucas Rossi <lucasrossi@fetch.ly>
2025-01-22 08:32:32 -0300

    update to v7

--- db/migrate/20250106110445_create_movies.rb
+++ db/migrate/20250106110445_create_movies.rb
@@ -1,4 +1,4 @@
-class CreateMovies < ActiveRecord::Migration[8.0]
+class CreateMovies < ActiveRecord::Migration[7.1]
   def change
     create_table :movies do |t|
       t.string :name
--- db/migrate/20250108103759_add_fields_to_movies.rb
+++ db/migrate/20250108103759_add_fields_to_movies.rb
@@ -1,4 +1,4 @@
-class AddFieldsToMovies < ActiveRecord::Migration[8.0]
+class AddFieldsToMovies < ActiveRecord::Migration[7.1]
   def change
     add_column :movies, :description, :text
     add_column :movies, :released_on, :date
