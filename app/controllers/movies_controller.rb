67d304876413488db475a579eb5db3bcb7de81ec
Lucas Rossi <lucasrossi@fetch.ly>
2025-01-23 09:15:50 -0300

    add movie creation feature

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,15 +14,28 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
   
-    movie_params = params.require(:movie).permit(
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(
         :name,
         :description,
         :rating,
         :released_on,
         :total_gross
       )
-  
-    @movie.update(movie_params)
-    redirect_to @movie
-  end
+    end
 end
