70671749907873e58c40cc8655348291ba71b224
Lucas Rossi <lucasrossi@fetch.ly>
2025-01-23 08:46:31 -0300

    add patch route for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,19 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+  
+    movie_params = params.require(:movie).permit(
+        :name,
+        :description,
+        :rating,
+        :released_on,
+        :total_gross
+      )
+  
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
 end
