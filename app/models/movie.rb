849b30dfbb619716a6d8b8e94ec66c416c4c69f1
Lucas Rossi <lucasrossi@fetch.ly>
2025-01-24 08:53:40 -0300

    add new movie methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,27 @@
 class Movie < ApplicationRecord
+  @@flop_threshold = 400_000_000
+
+  def self.released
+    where("released_on <= ?", Time.now).order("released_on desc")
+  end
+
+  def self.upcoming
+    where("released_on > ?", Time.now).order("released_on asc")
+  end
+
+  def self.flopped
+    where("total_gross < ?", @@flop_threshold)
+  end
+
+  def self.succeeded
+    where("total_gross >= ?", @@flop_threshold)
+  end
+
   def flop?
-    total_gross.blank? || total_gross < 400_000_000
+    total_gross.blank? || total_gross < @@flop_threshold
+  end
+
+  def success?
+    total_gross >= @@flop_threshold
   end
 end
