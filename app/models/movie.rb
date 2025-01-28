496bbc779fbef43f220972f7177fe5ea7be3e2f4
Lucas Rossi <lucasrossi@fetch.ly>
2025-01-28 08:05:10 -0300

    add movies validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,15 @@
 class Movie < ApplicationRecord
-  @@flop_threshold = 400_000_000
+  FLOP_THRESHOLD = 400_000_000
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :name, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 20 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, inclusion: { in: RATINGS }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
 
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
@@ -10,18 +20,18 @@ class Movie < ApplicationRecord
   end
 
   def self.flopped
-    where("total_gross < ?", @@flop_threshold)
+    where("total_gross < ?", FLOP_THRESHOLD)
   end
 
   def self.succeeded
-    where("total_gross >= ?", @@flop_threshold)
+    where("total_gross >= ?", FLOP_THRESHOLD)
   end
 
   def flop?
-    total_gross.blank? || total_gross < @@flop_threshold
+    total_gross.blank? || total_gross < FLOP_THRESHOLD
   end
 
   def success?
-    total_gross >= @@flop_threshold
+    total_gross >= FLOP_THRESHOLD
   end
 end
