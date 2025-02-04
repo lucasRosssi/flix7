5ca5a96967f02f4c6ff3add78a2a6f16edc29298
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-04 07:33:37 -0300

    edit and delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,25 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_url, status: :see_other, notice: "Account successfully deleted"
+  end
   
   private
   
