0bfd9ca710e56459b92040b0ff6718c96e476469
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-05 07:26:20 -0300

    create auth gatekeeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  private
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    helper_method :current_user
+
+    def require_signin
+      unless current_user
+        flash[:warning] = "Please sign in first."
+        redirect_to signin_url
+      end
+    end
 end
