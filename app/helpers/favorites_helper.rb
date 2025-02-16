03f961a35619e2fd03e565f7dd01e462bf622cca
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-16 10:42:38 -0300

    handle favorites

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,9 @@
 module FavoritesHelper
+  def fave_or_unfave_button(movie, favorite)
+    if favorite
+      button_to "❤️", movie_favorite_path(movie, favorite), method: :delete
+    else
+      button_to "🤍", movie_favorites_path(movie)
+    end
+  end
 end
