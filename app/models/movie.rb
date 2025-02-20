c7f47a0c1e9dc0058b091394f81c96d953621f96
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-20 08:46:10 -0300

    validate image

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -19,6 +19,7 @@ class Movie < ApplicationRecord
   validates :description, length: { minimum: 20 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
   validates :rating, inclusion: { in: RATINGS }
+  validate :acceptable_image
 
   scope :released, -> { where("released_on <= ?", Time.now).order("released_on desc") }
   scope :upcoming, -> { where("released_on > ?", Time.now).order("released_on asc") }
@@ -53,4 +54,16 @@ class Movie < ApplicationRecord
       self.slug = name.parameterize
     end
 
+    def acceptable_image
+      return unless main_image.attached?
+    
+      unless main_image.blob.byte_size <= 1.megabyte
+        errors.add(:main_image, "is too big")
+      end
+    
+      acceptable_types = ["image/jpeg", "image/png"]
+      unless acceptable_types.include?(main_image.blob.content_type)
+        errors.add(:main_image, "must be a JPEG or PNG")
+      end
+    end
 end
